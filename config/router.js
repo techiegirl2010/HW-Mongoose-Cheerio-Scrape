35cbac44e7225e647480d4f42144c1916f6a1e9a
Joyce Buettner <iamtechiegirl@cox.net>
2019-08-15 14:25:55 -0700

    setup after tutor session

---
+++ config/router.js
@@ -0,0 +1,51 @@
+const express =require("express");
+const db = require("../models");
+const axios = require("axios");
+
+const router = express.Router();
+router.get("/", (req, res) => {
+    res.render("home");
+})
+router.get("/scrape", (req, res){
+    console.log("inside the scrape route");
+    var headlines = [];
+    axios.get("https://www.npr.org").then(function(response) {
+
+    // Load the Response into cheerio and save it to a variable
+    // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+    var $ = cheerio.load(response.data);
+
+    // An empty array to save the data that we'll scrape
+    var results = [];
+
+    // With cheerio, find each h3-tag with the "title" class
+    $("h3.title").each(function(i, element) {
+
+        // Save the text of the element in a "title" variable
+        var title = $(element).text();
+
+        // In the currently selected element, look at its child elements (i.e., its a-tags),
+        // then save the values for any "href" attributes that the child elements may have
+        var link = $(element).().attr("href");
+
+        // Save these results in an object that we'll push into the results array we defined earlier
+        var headlinesToAdd = {
+            title: title,
+            link: link,
+            saved: false
+        }
+        headlines.push(headlinesToAdd);
+    });
+
+    // Log the results once you've looped through each of the elements found with cheerio
+    console.log(results);
+    db.Headline.create(headlines)
+    .then(function(dbHeadlines){
+        res.render("home", {db_headlines: dbHeadlines});
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+    });
+    res.send("complete");
+})
---
+++ server.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var exphbs = require('express-handlebars');
+
+// Require all models
+var db = require("./models");
+
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/scrapeHeadlines", { useNewUrlParser: true });
+
+// Routes
+
+var routes = require('./config');
+app.use('/', routes);
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
+  -080
+  /*
