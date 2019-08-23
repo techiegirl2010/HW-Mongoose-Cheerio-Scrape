acc267412a723ce34927d4cfaf7eb5ec33f9c523
Joyce Buettner <iamtechiegirl@cox.net>
2019-08-23 14:01:26 -0700

    changed port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var exphbs = require('express-handlebars');
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
